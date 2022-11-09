6a71500e2738013539cc14bcf3992b6f6348344f
Max Pestov <pestov.maxik@gmail.com>
2022-11-09 22:29:09 +0400

    Oauth (#4)
    * spring oauth server deps added
    
    * base configuration

---
+++ web/src/main/java/com/unitalegio/sso/web/config/SecurityConfig.java
@@ -0,0 +1,102 @@
+package com.unitalegio.sso.web.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.config.ProviderSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(httpSecurity);
+
+        httpSecurity
+                .exceptionHandling(
+                        // Redirect to the login page when not authenticated from the
+                        // authorization endpoint
+                        (exceptions) -> exceptions.authenticationEntryPoint(
+                                new LoginUrlAuthenticationEntryPoint("/login"))
+                );
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        // TODO: Create RegisteredClientRepository within DB #3;
+
+        RegisteredClient tempRegisteredClient = RegisteredClient
+                .withId("tempId")
+                .clientSecret("{noop}tempSecret")
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_POST)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/messaging-client-oidc")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope("temp.temp1")
+                .scope("temp.temp2")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build())
+                .build();
+
+        return new InMemoryRegisteredClientRepository(tempRegisteredClient);
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAKey rsaKey = new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return new ImmutableJWKSet<>(jwkSet);
+    }
+
+    private KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            throw new IllegalStateException("Something went wrong while generating RSA KeyPair \n" + e.getMessage());
+        }
+        return keyPair;
+    }
+
+    @Bean
+    public ProviderSettings providerSettings() {
+        // TODO: Inject provider parameters from configuration yaml;
+        return ProviderSettings.builder().issuer("unitalegio.ml").build();
+    }
+}
--- web/src/test/java/com/unitalegio/sso/web/WebApplicationTests.java
+++ web/src/test/java/com/unitalegio/sso/web/WebApplicationTests.java
@@ -3,11 +3,12 @@ package com.unitalegio.sso.web;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 @SpringBootTest
 class WebApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
