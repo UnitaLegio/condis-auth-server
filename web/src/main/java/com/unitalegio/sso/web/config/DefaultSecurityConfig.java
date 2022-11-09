69f590d22a67bc4b29d0d5f810e626a8c8e57783
Max Pestov <pestov.maxik@gmail.com>
2022-11-09 21:46:45 +0400

    base configuration

---
+++ web/src/main/java/com/unitalegio/sso/web/config/DefaultSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.unitalegio.sso.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
+public class DefaultSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        // Form login handles the redirect to the login page from the
+        // authorization server filter chain
+        httpSecurity.authorizeHttpRequests(authorize ->
+                        authorize.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        // TODO: Implement real userDetailService #1
+        // TODO: Create bean of passwordEncoder #2
+        UserDetails tempUser = User.withDefaultPasswordEncoder()
+                .username("tempUser")
+                .password("password")
+                .roles("tempRole")
+                .build();
+        return new InMemoryUserDetailsManager(tempUser);
+    }
+}
